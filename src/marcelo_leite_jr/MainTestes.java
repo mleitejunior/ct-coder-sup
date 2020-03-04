eec0292ac06fdfd989a3d3cfcdc66b0c3847e32b
mleitejunior <mleitejunior@gmail.com>
2020-03-04 10:43:49 -0300

    Acidionado Readme_exemplo.md

--- src/marcelo_leite_jr/MainTestes.java
+++ src/marcelo_leite_jr/MainTestes.java
@@ -1,7 +1,12 @@
 package marcelo_leite_jr;
 
+import java.util.Scanner;
+
 public class MainTestes {
     public static void main(String[] args) {
-        System.out.println("TESTE");
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+        System.out.println("O numero digitado é " + num);
     }
 }
