4f9a2cce84fa8634c01aa6329e993e9fe739d4a4
brunadelmourosilva <61791877+brunadelmourosilva@users.noreply.github.com>
2020-03-23 20:10:47 -0300

    Create URI1001.java

---
+++ solutions/bruna_delmouro/URI1001.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class URI1001 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+
+        System.out.println("X = " + (num1 + num2));
+
+    }
+}
