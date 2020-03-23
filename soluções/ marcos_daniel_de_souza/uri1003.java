aac5e5d285b532264daa2cc4298302ee4f6f6aa4
chapollin <61791223+chapollin@users.noreply.github.com>
2020-03-23 19:33:19 -0300

    Create uri1003.java

---
+++ soluções/ marcos_daniel_de_souza/uri1003.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+public static void main(String[] args){
+Scanner in = new Scanner(System.in);
+int A = in.nextInt();
+int B = in.nextInt();
+int SOMA = A+B;
+System.out.println("SOMA = "+SOMA);
+}
+}
