21f094e40ec572359f54ef7c72905a92286929f4
Marcelo Leite <57191522+mleite64@users.noreply.github.com>
2020-03-23 19:16:47 -0300

    Update URI1001.java
    Tinha enviado URI1002.

--- solutions/marcelo_leite/URI1001.java
+++ solutions/marcelo_leite/URI1001.java
@@ -1,17 +1,18 @@
 import java.io.IOException;
-import java.util.Scanner; 
-
+import java.util.Scanner;
+ 
 public class Main {
  
     public static void main(String[] args) throws IOException {
+    
+    Scanner sc = new Scanner(System.in);
         
-        Scanner sc = new Scanner(System.in);
-        double N = 3.14159d;
-        double raio = sc.nextDouble();
-       
-            System.out.println("A=" + String.format("%.4f", + N * raio * raio));
-       
- 
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        int X = A + B;
+        System.out.println("X = " + X);
+        
     }
  
 }
