9484396eae5cda47b74d0b1356948ab33663b4e8
brunadelmourosilva <61791877+brunadelmourosilva@users.noreply.github.com>
2020-03-23 21:07:43 -0300

    Update URI1001.java

--- solutions/bruna_delmouro/URI1001.java
+++ solutions/bruna_delmouro/URI1001.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class URI1001 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
         Scanner scan = new Scanner(System.in);
 
