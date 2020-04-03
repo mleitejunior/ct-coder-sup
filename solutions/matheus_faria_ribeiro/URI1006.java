bf2d49b5da20d43df3b9efe8456ff7ea0e102dff
Matheusfr17x <61791461+Matheusfr17x@users.noreply.github.com>
2020-04-03 17:45:18 -0300

    Create URI1006.java

---
+++ solutions/matheus_faria_ribeiro/URI1006.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ Scanner sc= new Scanner(System.in);
+         
+     
+     double A = sc.nextDouble()/5;
+     double B = sc.nextDouble()/3.33;
+     double C = sc.nextDouble()/2;
+     double MEDIA=(A+B+C);
+             
+     System.out.printf("MEDIA = %.1f\n", MEDIA);
+     
+    }
+   
+    }
