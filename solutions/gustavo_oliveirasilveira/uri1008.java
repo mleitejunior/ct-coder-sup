7646ed22146080ccd497a1b4e237a2de9d58303b
Guuh258 <57643517+capitain258@users.noreply.github.com>
2020-04-04 17:29:00 -0300

    Create uri1008.java

---
+++ solutions/gustavo_oliveirasilveira/uri1008.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+    
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+        DecimalFormat dc = new DecimalFormat("#.00");
+        
+        int num = scan.nextInt();
+        int numHoras = scan.nextInt();
+        double salarioHora = scan.nextDouble();
+        double total;
+        
+        total = numHoras * salarioHora;
+        System.out.println("NUMBER = " + num);
+        System.out.println("SALARY = U$ " + dc.format(total));
+ 
+    }
+ 
+}
