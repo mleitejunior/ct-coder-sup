bc09ab39a653895bdfd8ab4cabf595e523e3ca86
Marcelo Leite <57191522+mleite64@users.noreply.github.com>
2020-04-15 18:37:38 -0300

    Update URI1020Ativ3.java
    alteração da classe para Main

--- solutions/marcelo_leite/URI1020Ativ3.java
+++ solutions/marcelo_leite/URI1020Ativ3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class URI1020 {
+public class Main {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         // tipo de dados int por nao precisar de casas decimais
