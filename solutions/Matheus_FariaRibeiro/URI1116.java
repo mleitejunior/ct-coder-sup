27655bb90e699e19d84aa2ee6a23589bd26f2e66
Matheusfr17x <61791461+Matheusfr17x@users.noreply.github.com>
2020-04-19 22:56:56 -0300

    Create URI1116.java

---
+++ solutions/Matheus_FariaRibeiro/URI1116.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Main {
+
+ 
+    public static void main(String[] args) throws IOException {
+ 
+      
+Scanner input  = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        DecimalFormat df = new DecimalFormat("0.0");
+        int n;
+        double a, b;
+        n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            a = input.nextInt();
+            b = input.nextInt();
+            if(b!=0){
+                System.out.println( df.format (a/b));
+                
+
+            }
+
+            if(b==0){
+            System.out.println("divisao impossivel");
+            }
+
+        }
+
+    }
